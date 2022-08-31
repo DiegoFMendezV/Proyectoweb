fb4a1bf51ef65a89c4332b10f2d2f868cd6c1bcc
unknown <SENA@MOSAPRCBAFSD178.FormacionSena.RED>
2022-08-30 19:57:56 -0500

    version2

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,9 @@
 //Inicializamos la libreria AOS
 AOS.init();
+let menu = document.getElementById('menu');
+let menu_bar = document.getElementById('menu_bar');
+menu_bar.addEventListener('click', function(){/*Escuchador de los eventos. cuando escuche el evento click va a ajecutar la funcion()*/
+    menu.classList.toggle('menu-toggle')
+});
+
+/*por medio de js seleccionamos nuestros elementos para traer el menu devuelta*/
